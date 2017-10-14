64378688d18f674a3f6523d6d2d382209213a7c2
Makhout Nabil <makhout.nabil@gmail.com>
2017-10-14 19:09:03 +0200

    Changed the cancel and confirm button of position

--- app/pickerHandle/handle.js
+++ app/pickerHandle/handle.js
@@ -59,17 +59,17 @@ class Handle extends Component {
           <View style={[styles.nav, this.props.navStyle]}>
             <TouchableOpacity style={styles.cancel} onPress={() => {this.props.cancelChose(false, 'cancel');
             }}>
-            <View style={styles.pickerNameContainer}>
-              <Text style={[styles.pickerName,this.props.pickerNameStyle]}>{this.props.pickerName}</Text>
-            </View>
-              <TouchableOpacity  style={styles.confirm} onPress={() => {this.props.confirmChose();}}>
-                <Text className={'confirm'}
-                      style={[styles.confirmBtnStyle,this.props.confirmBtnStyle]} >{this.props.confirmBtnText}</Text>
-              </TouchableOpacity>
               <Text
                 className={'cancel'}
                 style={[styles.cancelBtnStyle,this.props.cancelBtnStyle]}
               >{this.props.cancelBtnText}</Text>
+            </TouchableOpacity>
+            <View style={styles.pickerNameContainer}>
+              <Text style={[styles.pickerName,this.props.pickerNameStyle]}>{this.props.pickerName}</Text>
+            </View>
+            <TouchableOpacity  style={styles.confirm} onPress={() => {this.props.confirmChose();}}>
+              <Text className={'confirm'}
+                    style={[styles.confirmBtnStyle,this.props.confirmBtnStyle]} >{this.props.confirmBtnText}</Text>
             </TouchableOpacity>
           </View>
     );
